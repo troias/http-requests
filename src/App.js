be8e5c3fdd171cc85f2f0ca73b7985851239525f
troy <hunter007nz@gmail.com>
2022-07-04 21:10:52 +1200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,58 +1,65 @@
-import React, {useEffect, useState, useCallback} from 'react';
-import AddMovie from './components/AddMovie';
-import MoviesList from './components/MoviesList';
-import './App.css';
-
+import React, { useEffect, useState, useCallback } from "react"
+import AddMovie from "./components/AddMovie"
+import MoviesList from "./components/MoviesList"
+import "./App.css"
 
 function App() {
-
   const [movies, setMovies] = useState([])
-  const [loading, setLoading]= useState(false)
-  const [ error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState("")
 
   const fetchMovieshandler = useCallback(async () => {
     setLoading(true)
     setError(null)
-    
+
     try {
-      const req = await fetch('https://react-http-6bc54-default-rtdb.firebaseio.com/movies.json')
+      const req = await fetch(
+        "https://react-http-6bc54-default-rtdb.firebaseio.com/movies.json"
+      )
       if (!req.ok) {
-        throw new Error('Something went wrong')
+        throw new Error("Something went wrong")
       }
       const response = await req.json()
       console.log(response)
 
       const loadedMovies = []
       for (const key in response) {
-        loadedMovies.push({ 
+        loadedMovies.push({
           id: key,
-          title: response[key].title, 
-          openingText: response[key].openingText, 
-          releaseDate: response[key].releaseDate, 
+          title: response[key].title,
+          openingText: response[key].openingText,
+          releaseDate: response[key].releaseDate,
         })
       }
-        setMovies(loadedMovies)
-        setLoading(false)
-    } catch(error) {
+      setMovies(loadedMovies)
+      setLoading(false)
+    } catch (error) {
       setError(error.message)
-    
     }
     setLoading(false)
   }, [])
 
-  useEffect(() =>{
+  useEffect(() => {
     fetchMovieshandler()
   }, [fetchMovieshandler])
 
-  const addMovieHandler = async (movie)  => {
-    const req = await fetch("https://react-http-6bc54-default-rtdb.firebaseio.com/movies.json", { 
-      method: 'POST',
-      body: JSON.stringify(movie), 
-      headers: {'Content-Type': 'application/json'}
-    })
-    
+  const addMovieHandler = async (movie) => {
+    setLoading(true)
+    const req = await fetch(
+      "https://react-http-6bc54-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+
     const res = await req.json
-    console.log(res);
+    if (!res.ok) {
+      throw new Error("Something went wrong")
+    }
+
+    console.log(res)
   }
 
   let content = <p> found no movies </p>
@@ -60,42 +67,41 @@ function App() {
   if (movies.length > 0) {
     content = <MoviesList movies={movies} />
   }
-  
+
   if (error) {
     content = <p> {error} </p>
   }
-  
+
   if (loading) {
     content = <p> loading </p>
   }
 
   const fetchStarWarsMoviesHandler = async () => {
-
-    setLoading(true);
-    setError(null);
-    setMovies([]);
+    setLoading(true)
+    setError(null)
+    setMovies([])
 
     try {
-      const req = await fetch('https://swapi.dev/api/films/')
+      const req = await fetch("https://swapi.dev/api/films/")
       if (!req.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!")
       }
 
-      const res= await req.json()
+      const res = await req.json()
       const data = res.results.map((result) => {
         return {
-        id: result.episode_id, 
-        title: result.title, 
-        releaseDate: result.release_date, 
-        openingText: result.opening_crawl  
-      }})
+          id: result.episode_id,
+          title: result.title,
+          releaseDate: result.release_date,
+          openingText: result.opening_crawl,
+        }
+      })
 
       setMovies(data)
-  
+
       console.log(data)
     } catch (error) {
       setError(error.message)
- 
     }
     setLoading(false)
   }
@@ -103,20 +109,19 @@ function App() {
   return (
     <React.Fragment>
       <section>
-      <AddMovie onAddMovie={addMovieHandler} />
+        <AddMovie onAddMovie={addMovieHandler} />
       </section>
       <section>
         <button onClick={fetchMovieshandler}>Fetch Movies</button>
       </section>
       <section>
-        <button onClick={fetchStarWarsMoviesHandler}>Fetch Starwars movies</button>
+        <button onClick={fetchStarWarsMoviesHandler}>
+          Fetch Starwars movies
+        </button>
       </section>
-      <section>
-    {content}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
-  );
+  )
 }
 
-export default App;
-
+export default App
